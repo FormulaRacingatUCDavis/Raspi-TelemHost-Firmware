c137b0e6b5f72e58ad6adf117ce11cac3e92e740
Ryan Tengan <ryantengan2004@gmail.com>
2026-01-17 15:02:47 -0500

    updated knobs test script

--- test/knobs.py
+++ test/knobs.py
@@ -1,21 +1,35 @@
 import os
 import can
 import cantools
+import random
 
 TESTKNOB = 1
 
-dbc_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'FE12.dbc'))
-db = cantools.database.load_file(dbc_path)
+db = cantools.database.load_file('FE12.dbc')
 bus = can.interface.Bus(channel='vcan0', interface='socketcan')
 
 knob = 0
 while knob <= 4095:
-    message = db.get_message_by_name('Dashboard_Knobs')
+    message = db.get_message_by_name('Inputs')
 
     if TESTKNOB == 1:
-        data = {'Knob1': knob, 'Knob2': 0}
+        data = {
+            'Dashboard_Knob1': knob, 
+            'Dashboard_Knob2': 0,
+            'Dashboard_DISPLAY_MODE': 0,
+            'Dashboard_TC': 0,
+            'Dashboard_MARKER': 0,
+            'Dashboard_OVERTAKE': 0
+            }
     elif TESTKNOB == 2:
-        data = {'Knob1': 0, 'Knob2': knob}
+        data = {
+            'Dashboard_Knob1': 0, 
+            'Dashboard_Knob2': knob,
+            'Dashboard_DISPLAY_MODE': 0,
+            'Dashboard_TC': 0,
+            'Dashboard_MARKER': 0,
+            'Dashboard_OVERTAKE': 0
+            }
             
     encoded = message.encode(data)
     msg = can.Message(arbitration_id=message.frame_id, data=encoded, is_extended_id=False)
