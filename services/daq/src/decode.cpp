f5c7b0e7d4a3c324f2f236a0523b30ced261fe14
Ryan Tengan <ryantengan2004@gmail.com>
2026-01-25 12:52:33 -0800

    added inverter module temperature bar chart

--- services/daq/src/decode.cpp
+++ services/daq/src/decode.cpp
@@ -23,11 +23,11 @@ namespace telem
                 struct fe12_db_vehicle_state_t msg;
                 fe12_db_vehicle_state_unpack(&msg, cap.frame.data, cap.frame.can_dlc);
 
-                j["dashboard_hv_requested"] = msg.dashboard_hv_requested;
-                j["dashboard_throttle1_level"] = msg.dashboard_throttle1_level;
-                j["dashboard_throttle2_level"] = msg.dashboard_throttle2_level;
-                j["dashboard_brake_level"] = msg.dashboard_brake_level;
-                j["dashboard_vcu_ticks"] = msg.dashboard_vcu_ticks;
+                j["dashboard_hv_requested"]   = fe12_db_vehicle_state_dashboard_hv_requested_decode(msg.dashboard_hv_requested);
+                j["dashboard_throttle1_level"] = fe12_db_vehicle_state_dashboard_throttle1_level_decode(msg.dashboard_throttle1_level);
+                j["dashboard_throttle2_level"] = fe12_db_vehicle_state_dashboard_throttle2_level_decode(msg.dashboard_throttle2_level);
+                j["dashboard_brake_level"]    = fe12_db_vehicle_state_dashboard_brake_level_decode(msg.dashboard_brake_level);
+                j["dashboard_vcu_ticks"]      = fe12_db_vehicle_state_dashboard_vcu_ticks_decode(msg.dashboard_vcu_ticks);
 
                 uint8_t state_key = fe12_db_vehicle_state_dashboard_state_decode(msg.dashboard_state);
                 j["dashboard_state"] = (VEHICLE_STATE.find(state_key) != VEHICLE_STATE.end()) 
@@ -36,18 +36,30 @@ namespace telem
 
                 break;
             }
-            case 0x0C0:
+            case 0xC0:
             {
                 struct fe12_db_torque_request_t msg;
                 fe12_db_torque_request_unpack(&msg, cap.frame.data, cap.frame.can_dlc);
 
-                j["dashboard_torque"] = msg.dashboard_torque;
-                j["dashboard_speed"] = msg.dashboard_speed;
-                j["dashboard_direction"] = msg.dashboard_direction;
-                j["dashboard_inverter_enable"] = msg.dashboard_inverter_enable;
-                j["dashboard_discharge_enable"] = msg.dashboard_discharge_enable;
-                j["dashboard_speed_mode_enable"] = msg.dashboard_speed_mode_enable;
-                j["dashboard_torque_limit"] = msg.dashboard_torque_limit;
+                j["dashboard_torque"] = fe12_db_torque_request_dashboard_torque_decode(msg.dashboard_torque);
+                j["dashboard_speed"] = fe12_db_torque_request_dashboard_speed_decode(msg.dashboard_speed);
+                j["dashboard_direction"] = fe12_db_torque_request_dashboard_direction_decode(msg.dashboard_direction);
+                j["dashboard_inverter_enable"] = fe12_db_torque_request_dashboard_inverter_enable_decode(msg.dashboard_inverter_enable);
+                j["dashboard_discharge_enable"] = fe12_db_torque_request_dashboard_discharge_enable_decode(msg.dashboard_discharge_enable);
+                j["dashboard_speed_mode_enable"] = fe12_db_torque_request_dashboard_speed_mode_enable_decode(msg.dashboard_speed_mode_enable);
+                j["dashboard_torque_limit"] = fe12_db_torque_request_dashboard_torque_limit_decode(msg.dashboard_torque_limit);
+
+                break;
+            }
+            case 0xA0:
+            {
+                struct cm200_db_m160_temperature_set_1_t msg;
+                cm200_db_m160_temperature_set_1_unpack(&msg, cap.frame.data, cap.frame.can_dlc);
+
+                j["inv_module_a_temp"] = cm200_db_m160_temperature_set_1_inv_module_a_temp_decode(msg.inv_module_a_temp);
+                j["inv_module_b_temp"] = cm200_db_m160_temperature_set_1_inv_module_b_temp_decode(msg.inv_module_b_temp);
+                j["inv_module_c_temp"] = cm200_db_m160_temperature_set_1_inv_module_c_temp_decode(msg.inv_module_c_temp);
+                j["inv_gate_driver_board_temp"] = cm200_db_m160_temperature_set_1_inv_gate_driver_board_temp_decode(msg.inv_gate_driver_board_temp);
 
                 break;
             }
--- services/web/public/app.js
+++ services/web/public/app.js
@@ -3,37 +3,56 @@ import Chart from 'chart.js/auto';
 import 'chartjs-adapter-moment';
 import mqtt from 'mqtt';
 
-const client = mqtt.connect('ws://192.168.137.249:9001');
+/* Connect to MQTT broker from web browser using WebSockets */
 
-let scatterChart;
+const client = mqtt.connect('ws://192.168.137.249:9001');
 
 client.on('connect', () => {
   console.log('Connected to MQTT broker.');
+  client.subscribe('can/A0');
   client.subscribe('can/C0');
 });
+
+/* Graph live data */
+
+let lineChart;
+let barChart;
 
 const MAX_POINTS = 1000;
 
 client.on('message', (topic, message) => {
-  if (topic === 'can/C0') {
-    const data = JSON.parse(message.toString());
+  if (topic === 'can/A0') {
+    const msg = JSON.parse(message.toString());
 
-    if (scatterChart) {
-      const dataset = scatterChart.data.datasets[0].data;
+    if (barChart) {
+      const dataset = barChart.data.datasets[0].data;
+
+      dataset[0] = msg.inv_module_a_temp;
+      dataset[1] = msg.inv_module_b_temp;
+      dataset[2] = msg.inv_module_c_temp;
+
+      barChart.update();
+    }
+  }
+  else if (topic === 'can/C0') {
+    const msg = JSON.parse(message.toString());
+
+    if (lineChart) {
+      const dataset = lineChart.data.datasets[0].data;
       
-      dataset.push({ x: data.timestamp, y: data.dashboard_torque });
+      dataset.push({ x: msg.timestamp, y: msg.dashboard_torque });
 
       if (dataset.length > MAX_POINTS) {
         dataset.splice(0, dataset.length - MAX_POINTS);
       }
 
-      scatterChart.update('none');
+      lineChart.update('none');
     }
   }
 });
 
 (async function() {
-  const data = {
+  const lineData = {
     datasets: [{
       label: 'No Data Selected',
       data: [],
@@ -41,11 +60,11 @@ client.on('message', (topic, message) => {
     }],
   };
 
-  scatterChart = new Chart(
-    document.getElementById('scatterChart'),
+  lineChart = new Chart(
+    document.getElementById('lineChart'),
     {
     type: 'line',
-    data: data,
+    data: lineData,
     options: {
       parsing: false,
       normalized: true,
@@ -61,7 +80,6 @@ client.on('message', (topic, message) => {
           title: {
             display: true,
             text: 'Time',
-            font: { size: 16 }
           }
         },
         y: {
@@ -69,7 +87,6 @@ client.on('message', (topic, message) => {
           title: {
             display: true,
             text: 'No Data Selected',
-            font: { size: 16 }
           }
         }
       },
@@ -80,4 +97,44 @@ client.on('message', (topic, message) => {
       }
     }
   });
+
+  const labels = ['Module A', 'Module B', 'Module C'];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Inverter Module Temperatures',
+      data: [null, null, null],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  barChart = new Chart(
+    document.getElementById('barChart'),
+    {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Temperature [C]',
+            }
+          }
+        }
+      }
+    }
+  )
+
 })();
