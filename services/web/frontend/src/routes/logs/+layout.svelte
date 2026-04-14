64a6c2f1969871a4ebce94b6206081ccb8deda81
Ryan Tengan <ryantengan2004@gmail.com>
2026-04-13 19:32:19 -0500

    fixed bug: switching to logs/readings from logs/camera

--- services/web/frontend/src/routes/logs/+layout.svelte
+++ services/web/frontend/src/routes/logs/+layout.svelte
@@ -1,6 +1,10 @@
 <div style="display: flex; gap: 10px;">
-  <a href="/logs/readings"><button class="secondary">Readings</button></a>
-  <a href="/logs/camera"><button class="secondary">Camera</button></a>
+  <a href="/logs/readings" data-sveltekit-reload>
+    <button class="secondary">Readings</button>
+  </a>
+  <a href="/logs/camera" data-sveltekit-reload>
+    <button class="secondary">Camera</button>
+  </a>
 </div>
 <hr />
 <slot />
