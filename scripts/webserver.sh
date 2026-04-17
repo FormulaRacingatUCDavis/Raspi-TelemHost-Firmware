95be66da57204e9cead376e9a7d84acedaefa748
maximum138 <maxwellyhchen@gmail.com>
2026-04-17 13:37:26 -0700

    changed process to background

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/frucd/Raspi-TelemHost-Firmware/
-./services/daq/build/DAQ
+./services/daq/build/DAQ &
 
 source venv_webapp/bin/activate
 python3 services/web/backend/main.py
