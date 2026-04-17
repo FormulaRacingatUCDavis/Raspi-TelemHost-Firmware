742a33e9063e3e948fb2913b4a8d530f28ff155a
maximum138 <maxwellyhchen@gmail.com>
2026-04-17 13:34:53 -0700

    changed web shell script

--- scripts/webserver.sh
+++ scripts/webserver.sh
@@ -3,6 +3,5 @@
 cd /home/frucd/Raspi-TelemHost-Firmware/
 ./services/daq/build/DAQ
 
-cd web
 source venv_webapp/bin/activate
-python -m uvicorn main:app --host 192.168.105.2
+python3 services/web/backend/main.py
