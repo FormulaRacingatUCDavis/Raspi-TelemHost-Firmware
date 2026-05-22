0484ec9e0f4835141c5f0cbdd39f20c0f1abee04
Mazhu6 <mzhu396@gmail.com>
2026-05-22 02:38:43 -0700

    fixed spelling errors

--- scripts/update_dbc_files.sh
+++ scripts/update_dbc_files.sh
@@ -3,7 +3,7 @@
 cd /home/frucd/Raspi-TelemHost-Firmware/
 
 echo "removing old dbc source files"
-rm dashboard/src/cm200_db.c dashboard/inc/cm200_db.h dashboard/src/fe13_db.c dashboard/inc/fe13_db.h services/daq/src/cm200_db.c services/daq/inc/cm200_db.h services/daq/src/fe_13.c services/daq/inc/fe13_db.h
+rm dashboard/src/cm200_db.c dashboard/inc/cm200_db.h dashboard/src/fe13_db.c dashboard/inc/fe13_db.h services/daq/src/cm200_db.c services/daq/include/cm200_db.h services/daq/src/fe13_db.c services/daq/inlude/fe13_db.h
 
 echo "generating new dbc source files"
 python3 -m cantools generate_c_source --database-name cm200_db --output-directory dashboard/src resources/”20240129 Gen5 CAN DB.dbc”
